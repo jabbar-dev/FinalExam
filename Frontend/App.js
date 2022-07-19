e6e8c83a5f707a5fca92625b2ab30bc4d357955e
jabbar-dev <rashdijabbar@gmail.com>
2022-07-19 15:16:13 +0500

    Adds App.js code and Basic Structure in React

---
+++ Frontend/App.js
@@ -0,0 +1,61 @@
+
+import logo from './logo.svg';
+// import './App.css';
+import { useEffect, useState } from 'react';
+import { Button, Divider, Input,List,Modal, Typography } from 'antd';
+import axios from "axios"
+
+
+function App() {
+return (
+    <div className="App">
+        <div className="Navbar" style={{minWidth:"1000px",display:"flex",justifyContent:"space-between"}}>
+          <h2 style={{margin:"15px"}}> Favourites</h2>
+        <div style={{margin:"15px",minWidth:"500px"}}>
+        <Input type="text" name="search" value={value} onChange={searchBookmarks}/>
+        </div>
+        <div style={{margin:"15px"}}>
+          <Button type="primary" onClick={() => setAddModelOpen(true)}>+</Button>      
+        </div>
+        </div>
+
+
+      <>
+  {/* ADD */}
+
+      </>
+      <>
+ {/* Edit */}
+
+      </>
+
+    <>
+    <Divider orientation="left"></Divider>
+  
+    <div>
+      <ul style={{border:"1px solid gray",borderRadius:"10px",margin:"20px"}}>
+        {
+          bookmarks.length > 0 ? bookmarks.map(itm=> 
+            { return <div key={itm._id} style={{padding:"15px",display:"flex",justifyContent:'space-between'}}> 
+               <div>
+                <span style={{fontSize:"22px",margin:"8px",paddingRight:"20px" }}> {(itm.name+"").charAt(0)}</span> {itm.url}
+               </div>
+              <div >
+                <Button type="primary" onClick={()=>updateBookmark(itm._id)}> Edit</Button>
+                <Button type='primary' danger onClick={()=>deleteBookmark(itm._id)}> Delete</Button>
+              </div>
+            </div>}
+            ) : ""
+        }
+      </ul>
+      
+    </div>
+
+    <Divider orientation="left"></Divider>
+    
+    </>
+
+
+    </div>
+  );
+    }
